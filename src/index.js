3a3422d36fb2bc6567c1ec6d02c985260ed14bb9
Sean Wohlgemuth <sean@wohlgemuth.us>
2016-12-21 22:56:57 -0600

    rudementary set navigation for 0 to 1 sets

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@
 const Alexa = require('alexa-sdk');
 var APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
 
+var QuizletAPI = require('quizlet-api').QuizletAPI;
+var quizlet = new QuizletAPI('', '');
+
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     alexa.resources = languageStrings;
-    alexa.registerHandlers(entryPointHandlers, mainMenuHandlers);
+    alexa.registerHandlers(entryPointHandlers,
+        mainMenuHandlers,
+        queryQuizletHandlers,
+        confirmSingleSetHandlers,
+        setMenuHandlers);
     alexa.execute();
 };
 
-function parseToken(access_token){
-  var token = {};
-  token.user_id = access_token.split('|')[0];
-  token.access_token = access_token.substring(access_token.indexOf('|')+1);
-  return token;
+function parseToken(access_token) {
+    var token = {};
+    token.user_id = access_token.split('|')[0];
+    token.access_token = access_token.substring(access_token.indexOf('|') + 1);
+    return token;
 }
 
 var entryPointHandlers = {
@@ -23,15 +30,21 @@ var entryPointHandlers = {
         var speechOutput = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
         var accessToken = this.event.session.user.accessToken;
         this.handler.state = states.MAINMENU;
-        if(!accessToken){
+        if (!accessToken) {
             this.emitWithState('LinkAccountIntent');
         }
+        var token = parseToken(accessToken);
+        quizlet.access_token = token.access_token;
+        quizlet.user_id = token.user_id;
         this.emitWithState('PromptMainMenu', speechOutput);
     }
 }
 
 var states = {
-    MAINMENU: '_MAINMENU'
+    MAINMENU: '_MAINMENU',
+    QUERYQUERYQUIZLET: '_QUERYQUERYQUIZLET',
+    CONFIRMSINGLESET: '_CONFIRMSINGLESET',
+    SETMENU: '_SETMENU'
 };
 
 var mainMenuHandlers = Alexa.CreateStateHandler(states.MAINMENU, {
@@ -40,6 +53,14 @@ var mainMenuHandlers = Alexa.CreateStateHandler(states.MAINMENU, {
         var repromptSpeech = this.t("HELP_ME");
         this.emit(':ask', speechOutput, repromptSpeech);
     },
+    'SelectFavoriteSetIntent': function () {
+        this.handler.state = states.QUERYQUERYQUIZLET;
+        this.emitWithState('QueryFavoriteSetIntent');
+    },
+    'SelectSetIntent': function () {
+        this.handler.state = states.QUERYQUERYQUIZLET;
+        this.emitWithState('QuerytSetIntent');
+    },
     'LinkAccountIntent': function () {
         var accessToken = this.event.session.user.accessToken;
         if (!accessToken) {
@@ -47,7 +68,7 @@ var mainMenuHandlers = Alexa.CreateStateHandler(states.MAINMENU, {
             this.emit(':tellWithLinkAccountCard', speechOutput);
         }
         var token = parseToken(accessToken);
-        console.log('user_id: '+token.user_id+' access_token: '+token.access_token);
+        console.log('user_id: ' + token.user_id + ' access_token: ' + token.access_token);
         var speechOutput = this.t("LINKED", token.user_id, token.access_token);
         this.emit(':tell', speechOutput);
     },
@@ -70,13 +91,84 @@ var mainMenuHandlers = Alexa.CreateStateHandler(states.MAINMENU, {
     }
 });
 
+var queryQuizletHandlers = Alexa.CreateStateHandler(states.QUERYQUERYQUIZLET, {
+    'QueryFavoriteSetIntent': function () {
+        quizlet.getUserFavorites()
+            .then((data) => {
+                if (data.length == 0) {
+                    var speechOutput = "You do not have any favorite sets yet. ";
+                    this.handler.state = states.MAINMENU;
+                    this.emitWithState('PromptMainMenu', speechOutput);
+                }
+                this.attributes['sets'] = {};
+                this.attributes['sets'].data = data;
+                this.attributes['sets'].index = 0;
+                if (data.length == 1) {
+                    this.handler.state = states.CONFIRMSINGLESET;
+                    this.emitWithState('ConfirmSingleSetIntent');
+                } else if (data.length <= 4) {
+                    this.emit(':tell', "you have less than five sets. ");
+                } else {
+                    this.emit(':tell', "you have more than five sets. ");
+                }
+            })
+            .catch((err) => { console.log('error: ' + err) });
+    },
+    'QuerytSetIntent': function () {
+        quizlet.getUserSets()
+            .then((data) => {
+                if (data.length == 0) {
+                    var speechOutput = "You do not have any sets yet. Go to Quizlet dot com and add some sets to use.  Goodbye!";
+                    this.emit(':tell', speechOutput);
+                }
+                this.attributes['sets'] = {};
+                this.attributes['sets'].data = data;
+                this.attributes['sets'].index = 0;
+                if (data.length == 1) {
+                    this.handler.state = states.CONFIRMSINGLESET;
+                    this.emitWithState('ConfirmSingleSetIntent');
+                } else if (data.length <= 4) {
+                    this.emit(':tell', "you have less than five sets. ");
+                } else {
+                    this.emit(':tell', "you have more than five sets. ");
+                }
+            })
+            .catch((err) => { console.log('error: ' + err) });
+    }
+});
+
+var confirmSingleSetHandlers = Alexa.CreateStateHandler(states.CONFIRMSINGLESET, {
+    'ConfirmSingleSetIntent': function () {
+        var title = this.attributes["sets"].data[this.attributes['sets'].index].title;
+        this.emit(':ask', "Your set name is " + title + ".  Do you want to use this set? ");
+    },
+    'AMAZON.YesIntent': function () {
+        this.handler.state = states.SETMENU;
+        this.emitWithState('ConfirmSetIntent');
+    },
+    'AMAZON.NoIntent': function () {
+        this.handler.state = states.MAINMENU;
+        this.emitWithState('PromptMainMenu');
+    }
+});
+
+var setMenuHandlers = Alexa.CreateStateHandler(states.SETMENU, {
+    'ConfirmSetIntent': function () {
+        var set = this.attributes["sets"].data[this.attributes['sets'].index];
+        this.attributes["sets"].set = set;
+        this.attributes["sets"].data = undefined;
+        this.attributes["sets"].index = undefined;
+        this.emit(':tell', "You have chosen the set named " + set.title + ". ");
+    }
+});
+
 var languageStrings = {
     "en-US": {
         "translation": {
             "SKILL_NAME": "Quizlexa",
             "WELCOME_MESSAGE": "Welcome to %s. ",
             "HOW_CAN_I_HELP": "How can I help you? ",
-            "ASK_ME": "You can ask me to link an account. ",
+            "ASK_ME": "You can ask me to select a favorite set or to just select one of your sets. ",
             "HELP_ME": "For instructions on what you can say, please say help me. ",
             "HELP_MESSAGE": "%s, or, you can say exit...Now, %s",
             "STOP_MESSAGE": "Goodbye! ",
